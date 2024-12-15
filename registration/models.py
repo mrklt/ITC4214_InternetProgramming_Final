08387dfcaa8e6e37248b21eb4583f828d0ddf616
mrklt <m.markoulatou@acg.edu>
2024-12-15 17:21:06 +0200

    final commit <3

--- registration/models.py
+++ registration/models.py
@@ -5,5 +5,5 @@ from django.contrib.auth.models import User
 def user_is_creator(self):
     return self.groups.filter(name='Creator').exists()
 
-# Add the property dynamically to the User model
+
 User.add_to_class('is_creator', property(user_is_creator))
